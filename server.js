fe9fe42c3f001ccee6a3b7a0b6c3928c60e0207f
Skitsanos <info@skitsanos.com>
2019-04-08 12:43:22 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+
+const meta = require('./package');
+const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+
+class Application
+{
+    constructor()
+    {
+        const configFile = path.join(__dirname, 'config.json');
+        if (!fs.existsSync(configFile))
+        {
+            console.error(`config.json is not found in ${__dirname}`);
+            process.exit(1);
+        }
+        else
+        {
+            this.config = require('./config');
+        }
+    }
+
+    run()
+    {
+        program
+            .version(`${meta.version}`, '-v, --version')
+            .usage('-hostname')
+            .option('-h, --hostname <hostname>', 'hostname')
+            .parse(process.argv);
+
+        if (!program.hostname)
+        {
+            console.log('Missing hostname');
+            return;
+        }
+
+        const url = 'https://' + this.config.username + ':' + this.config.token + '@api.name.com/v4/domains/' + this.config.domain;
+
+        request.get('http://api.skitsanos.com/getip', (e, r, b) =>
+        {
+            let res = JSON.parse(b);
+            let ip = res.result;
+
+            console.log('Setting ' + host + '.' + domain + ' to ' + ip + '...');
+
+            request.get(url + '/records', (error, response, body) =>
+                {
+                    let doc = JSON.parse(body);
+
+                    if (!doc.records.some(element => element.host === host))
+                    {
+                        //create hostname
+                        request.post(url + '/records', {
+                            json: {
+                                host: host,
+                                type: 'A',
+                                answer: ip,
+                                ttl: 300
+                            }
+                        }, (e, r, b) =>
+                        {
+                            if (b.answer === ip)
+                            {
+                                console.log('Host has been updated');
+                            }
+                            else
+                            {
+                                console.log('Failed to update host');
+                            }
+                        });
+                    }
+                    else
+                    {
+                        //update hostname by id
+
+                        let id = doc.records.find(element => element.host === host).id;
+
+                        request.put(url + '/records/' + id, {
+                            json: {
+                                host: host,
+                                type: 'A',
+                                answer: ip,
+                                ttl: 300
+                            }
+                        }, (e, r, b) =>
+                        {
+                            if (b.answer === ip)
+                            {
+                                console.log('Host has been updated');
+                            }
+                            else
+                            {
+                                console.log('Failed to update host');
+                            }
+                        });
+                    }
+                }
+            );
+        });
+
+    }
+}
+
+console.log(`${meta.name}, v.${meta.version} (${meta.description})`);
+
+const app = new Application();
+app.run();
